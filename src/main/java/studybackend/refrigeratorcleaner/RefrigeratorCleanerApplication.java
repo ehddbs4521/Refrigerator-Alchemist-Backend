27371c6d734817d0bd41be0f9a8f4bc6c472dfe0
ehddbs4521 <ehddbs452100@naver.com>
2024-02-07 21:31:49 +0900

    first-commit

---
+++ src/main/java/studybackend/refrigeratorcleaner/RefrigeratorCleanerApplication.java
@@ -0,0 +1,13 @@
+package studybackend.refrigeratorcleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefrigeratorCleanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefrigeratorCleanerApplication.class, args);
+	}
+
+}
