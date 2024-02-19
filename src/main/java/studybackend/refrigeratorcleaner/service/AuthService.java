8edfed517728a977b237aa80e1f00dddc58e2e72
ehddbs4521 <ehddbs452100@naver.com>
2024-02-19 16:07:15 +0900

    feat: Send Email Service

---
+++ src/main/java/studybackend/refrigeratorcleaner/service/AuthService.java
@@ -0,0 +1,58 @@
+package studybackend.refrigeratorcleaner.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import studybackend.refrigeratorcleaner.dto.VerifyEmailResonseDto;
+import studybackend.refrigeratorcleaner.repository.UserRepository;
+import studybackend.refrigeratorcleaner.util.EmailUtil;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final EmailUtil emailUtil;
+    private final AmazonS3Client amazonS3Client;
+
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public VerifyEmailResonseDto sendEmail(String email) throws MessagingException {
+
+        if (userRepository.existsByEmail(email)) {
+            throw new RuntimeException("이미 존재하는 이메일입니다.");
+        }
+
+        String randomNum = String.valueOf((new Random().nextInt(90000) + 10000));
+        LocalDateTime createTime = LocalDateTime.now();
+        LocalDateTime expireTime = LocalDateTime.now().plusMinutes(10);
+
+        emailUtil.sendEmail(email,randomNum);
+
+        VerifyEmailResonseDto verifyEmailResonseDto = VerifyEmailResonseDto.
+                builder()
+                .randomNum(randomNum)
+                .createTime(createTime)
+                .expireTime(expireTime)
+                .build();
+
+        return verifyEmailResonseDto;
+
+    }
+
+
+}
