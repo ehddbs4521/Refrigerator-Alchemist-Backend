3c658063e3f881b4b6c003c724afa89b913abafe
ehddbs4521 <ehddbs452100@naver.com>
2024-02-19 16:30:11 +0900

    feat: Implement UserDetailsService For Social ID Based Authentication

---
+++ src/main/java/studybackend/refrigeratorcleaner/login/service/LoginService.java
@@ -0,0 +1,30 @@
+package studybackend.refrigeratorcleaner.login.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import studybackend.refrigeratorcleaner.entity.User;
+import studybackend.refrigeratorcleaner.repository.UserRepository;
+
+
+@Service
+@RequiredArgsConstructor
+public class LoginService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String socialId) throws UsernameNotFoundException {
+        User user = userRepository.findBySocialId(socialId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다."));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
