c0b4dee32f5100172aec3eef09078023eb163306
ehddbs4521 <ehddbs452100@naver.com>
2024-02-19 16:43:11 +0900

    fix: annotation

--- src/main/java/studybackend/refrigeratorcleaner/login/filter/CustomJsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/studybackend/refrigeratorcleaner/login/filter/CustomJsonUsernamePasswordAuthenticationFilter.java
@@ -25,7 +25,7 @@ public class CustomJsonUsernamePasswordAuthenticationFilter extends AbstractAuth
     private static final String HTTP_METHOD = "POST"; // 로그인 HTTP 메소드는 POST
     private static final String CONTENT_TYPE = "application/json"; // JSON 타입의 데이터로 오는 로그인 요청만 처리
     private static final String USERNAME_KEY = "email"; // 회원 로그인 시 이메일 요청 JSON Key : "email"
-    private static final String SOCIAL_KEY = "socialType"; // 회원 로그인 시 이메일 요청 JSON Key : "email"
+    private static final String SOCIAL_KEY = "socialType"; // 회원 로그인 시 소셜ID 요청 JSON Key : "socialId"
     private static final String PASSWORD_KEY = "password"; // 회원 로그인 시 비밀번호 요청 JSon Key : "password"
     private static final AntPathRequestMatcher DEFAULT_LOGIN_PATH_REQUEST_MATCHER =
             new AntPathRequestMatcher(DEFAULT_LOGIN_REQUEST_URL, HTTP_METHOD); // "/login" + POST로 온 요청에 매칭된다.
