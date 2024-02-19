ded246bd8513ed3ffcaefc8c2e6186e3d7ddec4f
ehddbs4521 <ehddbs452100@naver.com>
2024-02-19 16:20:47 +0900

    feat: OAuth2User Info

---
+++ src/main/java/studybackend/refrigeratorcleaner/oauth/dto/OAuth2UserInfo.java
@@ -0,0 +1,18 @@
+package studybackend.refrigeratorcleaner.oauth.dto;
+
+import java.util.Map;
+
+public abstract class OAuth2UserInfo {
+
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public abstract String getId();
+
+    public abstract String getEmail();
+
+    public abstract String getImageUrl();
+}
