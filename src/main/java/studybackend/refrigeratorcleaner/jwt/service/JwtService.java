c2f56ca25557932114a14d1293546b89bb61f1c3
ehddbs4521 <ehddbs452100@naver.com>
2024-02-19 16:16:52 +0900

    feat: JWT Token Service

---
+++ src/main/java/studybackend/refrigeratorcleaner/jwt/service/JwtService.java
@@ -0,0 +1,147 @@
+package studybackend.refrigeratorcleaner.jwt.service;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import studybackend.refrigeratorcleaner.repository.UserRepository;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+@Getter
+@Slf4j
+public class JwtService {
+
+    private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    private static final String EMAIL_CLAIM = "email";
+    private static final String SOCIAL_TYPE = "socialType";
+    private static final String BEARER = "Bearer ";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 3;            // 유효기간 3시간
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 30;  // 유효기간 30일
+
+    private String secretKey;
+    private String accessHeader;
+    private String refreshHeader;
+    private final Key key;
+    private final UserRepository userRepository;
+
+    public JwtService(@Value("${jwt.secret-key}") String secretKey,
+                      @Value("${jwt.access-header}") String accessHeader,
+                      @Value("${jwt.refresh-header}") String refreshHeader,
+                      UserRepository userRepository) {
+        this.secretKey = secretKey;
+        this.accessHeader = accessHeader;
+        this.refreshHeader = refreshHeader;
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.userRepository = userRepository;
+    }
+    public String generateAccessToken(String email) {
+
+        long now = (new Date()).getTime();
+
+        Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+
+        return  Jwts.builder()
+                .setSubject(ACCESS_TOKEN_SUBJECT)
+                .claim(EMAIL_CLAIM, email)
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String generateRefreshToken() {
+
+        long now = (new Date()).getTime();
+
+        Date refreshTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+
+        return  Jwts.builder()
+                .setSubject(REFRESH_TOKEN_SUBJECT)
+                .setExpiration(refreshTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        setAccessTokenHeader(response, accessToken);
+        setRefreshTokenHeader(response, refreshToken);
+        log.info("Access Token, Refresh Token 헤더 설정 완료");
+    }
+
+    public void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
+        response.setHeader(accessHeader, accessToken);
+    }
+
+    public void setRefreshTokenHeader(HttpServletResponse response, String refreshToken) {
+        response.setHeader(refreshHeader, refreshToken);
+    }
+
+    public Optional<String> extractRefreshToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(refreshHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    }
+
+    public Optional<String> extractAccessToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(accessHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    }
+
+    public Optional<String> extractEmail(String accessToken) {
+        try {
+            // 토큰 유효성 검사하는 데에 사용할 알고리즘이 있는 JWT verifier builder 반환
+            return Optional.ofNullable(Jwts.parser()
+                            .setSigningKey(key)
+                            .build()
+                            .parseClaimsJws(accessToken)
+                            .getBody()
+                            .get(EMAIL_CLAIM, String.class)
+                    );
+        } catch (Exception e) {
+            log.error("액세스 토큰이 유효하지 않습니다.");
+            return Optional.empty();
+        }
+    }
+
+    public boolean isTokenValid(String token) { //토큰 검증
+        try {
+            Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+
+    public void updateRefreshToken(String socialId, String refreshToken) {
+
+        userRepository.findBySocialId(socialId)
+                .ifPresentOrElse(
+                        user -> {
+                            user.updateRefreshToken(refreshToken);
+                            userRepository.save(user); // 변경 사항을 저장
+                        },
+                        () -> new Exception("일치하는 회원이 없습니다.")
+                );
+    }
+}
