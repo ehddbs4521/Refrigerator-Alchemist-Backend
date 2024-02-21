d64cc16d1ffe2155d628210719d9c69b4d2b6c94
ehddbs4521 <ehddbs452100@naver.com>
2024-02-21 13:44:26 +0900

    fix: Add Valid Annotation

--- src/main/java/studybackend/refrigeratorcleaner/dto/UserRequestDto.java
+++ src/main/java/studybackend/refrigeratorcleaner/dto/UserRequestDto.java
@@ -1,5 +1,8 @@
 package studybackend.refrigeratorcleaner.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,8 +10,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public class UserRequestDto {
 
+    @Email
+    @NotNull
     private String email;
+
+    @NotNull
     private String nickName;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&]).{10,15}$",
+            message = "비밀번호는 문자, 숫자, 특수문자를 포함하여 10자리 이상 15자리 이하이어야 합니다.")
     private String password;
 
 
