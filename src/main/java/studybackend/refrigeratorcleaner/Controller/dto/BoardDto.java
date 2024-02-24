e6fd0e3157698f52edc40f8b45fa5869a2422b4f
changeme4585 <93308824+changeme4585@users.noreply.github.com>
2024-02-24 17:06:13 +0900

    BoardDto

---
+++ src/main/java/studybackend/refrigeratorcleaner/Controller/dto/BoardDto.java
@@ -0,0 +1,23 @@
+package studybackend.refrigeratorcleaner.Controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardDto {
+    @NotBlank(message = "제목을 입력해주세요.")
+    private String title;
+
+    @NotBlank(message = "본문을 입력해주세요.")
+    private String texts;
+
+    @Builder
+    public  BoardDto(String title,String texts){
+        this.title = title;
+        this.texts = texts;
+    }
+}
