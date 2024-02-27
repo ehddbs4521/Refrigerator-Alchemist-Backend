e96b36b8636fb426de506ee8bcfe581465ab9403
ehddbs4521 <ehddbs452100@naver.com>
2024-02-27 14:08:39 +0900

    feat: Email Request Dto

---
+++ src/main/java/studybackend/refrigeratorcleaner/dto/request/EmailRequest.java
@@ -0,0 +1,12 @@
+package studybackend.refrigeratorcleaner.dto.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class EmailRequest {
+
+    private final String email;
+    private final String emailType;
+}
