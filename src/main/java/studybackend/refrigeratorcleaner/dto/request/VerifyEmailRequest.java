ddea567b799ffeb28f2a5c5932fedf9ba202a849
ehddbs4521 <ehddbs452100@naver.com>
2024-03-01 17:29:10 +0900

    fix: Add EmailType

--- src/main/java/studybackend/refrigeratorcleaner/dto/request/VerifyEmailRequest.java
+++ src/main/java/studybackend/refrigeratorcleaner/dto/request/VerifyEmailRequest.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class VerifyEmailRequest {
 
     private String email;
+    private String emailType;
     private String socialType;
     private String randomNum;
     private String inputNum;
