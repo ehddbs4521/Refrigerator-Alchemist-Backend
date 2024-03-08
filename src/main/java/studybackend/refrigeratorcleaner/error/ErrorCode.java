0410d97f41d374e78730e28ff0477a2f3f0e1a4a
ehddbs4521 <ehddbs452100@naver.com>
2024-03-08 13:00:03 +0900

    feat: Custom Error Class

--- src/main/java/studybackend/refrigeratorcleaner/error/ErrorCode.java
+++ src/main/java/studybackend/refrigeratorcleaner/error/ErrorCode.java
@@ -14,9 +14,11 @@ public enum ErrorCode {
     NO_EXIST_USER_REFRESHTOKEN(HttpStatus.NOT_FOUND, "refreshToken이 없습니다."),
     NO_EXIST_USER_EMAIL(HttpStatus.NOT_FOUND, "이메일이 존재하지않습니다."),
     NO_EXIST_USER_SOCIALID(HttpStatus.NOT_FOUND, "socialId가 존재하지않습니다."),
-    NO_EXTRACT_EMAIL(HttpStatus.BAD_REQUEST, "토큰에서 email을 추출 할 수 없습니다.(잘못된 토큰)"),
+    NO_EXIST_USER_ACCESSTOKEN(HttpStatus.NOT_FOUND, "Access토큰이 존재하지않습니다."),
+    NO_EXTRACT_EMAIL(HttpStatus.UNAUTHORIZED, "토큰에서 email을 추출 할 수 없습니다.(잘못된 토큰)"),
+    NO_VALID_ACCESSTOKEN(HttpStatus.UNAUTHORIZED, "Access토큰이 유요하지 않습니다."),
     NO_EQUAL_JSON(HttpStatus.BAD_REQUEST, "data content-type이 json이 아닙니다."),
-    NO_EXTRACT_ACCESSTOKEN(HttpStatus.BAD_REQUEST, "토큰에서 AccessToken을 추출 할 수 없습니다.(잘못된 토큰)"),
+    NO_EXTRACT_ACCESSTOKEN(HttpStatus.UNAUTHORIZED, "토큰에서 AccessToken을 추출 할 수 없습니다.(잘못된 토큰)"),
     WRONG_CERTIFICATION_NUMBER(HttpStatus.BAD_REQUEST,"인증번호가 틀렸습니다."),
     EXPIRE_CERTIFICATION_NUMBER(HttpStatus.BAD_REQUEST,"인증번호가 만료되었습니다."),
     WRONG_PASSWORD(HttpStatus.BAD_REQUEST,"비밀번호를 재입력해주세요"),
@@ -26,6 +28,6 @@ public enum ErrorCode {
     ;
 
 
-    private final HttpStatus httpStatus;
+    private final HttpStatus status;
     private final String message;
 }
