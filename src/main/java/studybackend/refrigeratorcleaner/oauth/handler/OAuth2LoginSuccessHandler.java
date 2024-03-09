6ad77090beed826e0331d2e3f3fe68091113bb06
ehddbs4521 <ehddbs452100@naver.com>
2024-03-09 11:03:53 +0900

    feat: Save Token In Token DB

--- src/main/java/studybackend/refrigeratorcleaner/oauth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/studybackend/refrigeratorcleaner/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -2,21 +2,30 @@ package studybackend.refrigeratorcleaner.oauth.handler;
 
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+import studybackend.refrigeratorcleaner.dto.Role;
+import studybackend.refrigeratorcleaner.entity.Token;
 import studybackend.refrigeratorcleaner.entity.User;
+import studybackend.refrigeratorcleaner.error.CustomException;
 import studybackend.refrigeratorcleaner.jwt.service.JwtService;
 import studybackend.refrigeratorcleaner.oauth.dto.CustomOAuth2User;
-import studybackend.refrigeratorcleaner.dto.Role;
+import studybackend.refrigeratorcleaner.repository.TokenRepository;
 import studybackend.refrigeratorcleaner.repository.UserRepository;
 
-
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static studybackend.refrigeratorcleaner.error.ErrorCode.NO_EXIST_USER_SOCIALID;
+import static studybackend.refrigeratorcleaner.error.ErrorCode.NO_EXIST_USER_TOKEN;
 
 @Slf4j
 @Component
@@ -25,6 +34,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -33,18 +43,31 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
             CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
             if(oAuth2User.getRole() == Role.GUEST.getKey()) {
-                User user = userRepository.findBySocialId(oAuth2User.getSocialId()).get();
-                String accessToken = jwtService.generateAccessToken(oAuth2User.getEmail());
-                String refreshToken = jwtService.generateRefreshToken(oAuth2User.getEmail());
-                user.updateRefreshToken(refreshToken);
-                userRepository.save(user);
-                response.setHeader("socialId", oAuth2User.getSocialId());
-                response.setHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
-                jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
-                response.sendRedirect("localhost:3000/main");
+                User user = userRepository.findBySocialId(oAuth2User.getSocialId()).orElseThrow(() -> new CustomException(NO_EXIST_USER_SOCIALID));
+                Token token = user.getToken();
+                if (token == null) {
+                    throw new CustomException(NO_EXIST_USER_TOKEN);
+                }
+                String accessToken = jwtService.generateAccessToken(oAuth2User.getSocialId());
+                String refreshToken = jwtService.generateRefreshToken(oAuth2User.getSocialId());
+                token.updateTokens(accessToken, refreshToken);
+                tokenRepository.save(token);
+                String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/login-success")
+                        .queryParam("email",oAuth2User.getEmail())
+                        .queryParam("socialType",oAuth2User.getSocialType())
+                        .queryParam("socialId",oAuth2User.getSocialId())
+                        .queryParam("accessToken", accessToken)
+                        .build()
+                        .encode(StandardCharsets.UTF_8)
+                        .toUriString();
+
+                response.setHeader("Authorization-Access", accessToken);
+                response.addCookie(createCookie("Authorization-Refresh", refreshToken));
+                response.setStatus(HttpStatus.OK.value());
+
 
             } else {
-                loginSuccess(response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
+                loginSuccess(request,response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
             }
         } catch (Exception e) {
             throw e;
@@ -52,16 +75,33 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     }
 
-    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
-        String accessToken = jwtService.generateAccessToken(oAuth2User.getEmail());
-        String refreshToken = jwtService.generateRefreshToken(oAuth2User.getEmail());
+    private void loginSuccess(HttpServletRequest request,HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
 
-        log.info("accesstoken:{}", accessToken);
-        log.info("refreshtoken:{}", refreshToken);
-        log.info("socialId:{}",oAuth2User.getSocialId());
-        response.setHeader("socialId", oAuth2User.getSocialId());
-        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
-        jwtService.updateRefreshToken(oAuth2User.getSocialId(), refreshToken);
-        response.sendRedirect("localhost:3000/main");
+        String accessToken = jwtService.generateAccessToken(oAuth2User.getSocialId());
+        String refreshToken = jwtService.generateRefreshToken(oAuth2User.getSocialId());
+        jwtService.updateTokens(oAuth2User.getSocialId(), accessToken, refreshToken);
+
+        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/login-success")
+                .queryParam("email",oAuth2User.getEmail())
+                .queryParam("socialType",oAuth2User.getSocialType())
+                .queryParam("socialId",oAuth2User.getSocialId())
+                .queryParam("accessToken", accessToken)
+                .build()
+                .encode(StandardCharsets.UTF_8)
+                .toUriString();
+
+        response.setHeader("Authorization-Access", accessToken);
+        response.addCookie(createCookie("Authorization-Refresh", refreshToken));
+        response.setStatus(HttpStatus.OK.value());
+
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(1000 * 60 * 60 * 24 * 14);
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
