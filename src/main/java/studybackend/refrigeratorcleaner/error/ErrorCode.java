838bb507916030a5c1ddd9504d912cb2fbd0bd16
ehddbs4521 <ehddbs452100@naver.com>
2024-03-11 20:06:39 +0900

    feat: NO_EXIST_USER_NICKNAME Error Code

--- src/main/java/studybackend/refrigeratorcleaner/error/ErrorCode.java
+++ src/main/java/studybackend/refrigeratorcleaner/error/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     NO_EXIST_USER_EMAIL_SOCIALTYPE(HttpStatus.NOT_FOUND, "해당 socialType의 이메일이 존재하지 않습니다."),
     NO_EXIST_USER_REFRESHTOKEN(HttpStatus.NOT_FOUND, "refreshToken이 없습니다."),
     NO_EXIST_USER_EMAIL(HttpStatus.NOT_FOUND, "이메일이 존재하지않습니다."),
+    NO_EXIST_USER_NICKNAME(HttpStatus.NOT_FOUND, "닉네임이 존재하지않습니다."),
     NO_EXIST_USER_SOCIALID(HttpStatus.NOT_FOUND, "socialId가 존재하지않습니다."),
     NO_EXIST_USER_ACCESSTOKEN(HttpStatus.NOT_FOUND, "Access토큰이 존재하지않습니다."),
     NO_EXTRACT_EMAIL(HttpStatus.UNAUTHORIZED, "토큰에서 email을 추출 할 수 없습니다.(잘못된 토큰)"),
