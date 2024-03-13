45df4108cb960ae9a2172e1b59545cbaca419e17
ehddbs4521 <ehddbs452100@naver.com>
2024-03-13 20:44:41 +0900

    feat: Social Request Dto

--- src/main/java/studybackend/refrigeratorcleaner/jwt/dto/request/SocialIdRequest.java
+++ src/main/java/studybackend/refrigeratorcleaner/jwt/dto/request/SocialIdRequest.java
@@ -7,10 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ReIssueRequest {
+public class SocialIdRequest {
 
-    private String email;
-    private String socialType;
     private String socialId;
-    private String refreshToken;
 }
