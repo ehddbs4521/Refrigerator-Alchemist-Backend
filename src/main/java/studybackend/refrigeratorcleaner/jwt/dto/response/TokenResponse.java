c3b7401169dad30db03cd7fd764a71f26b81aaba
ehddbs4521 <ehddbs452100@naver.com>
2024-03-13 20:45:38 +0900

    feat: Token Response Dto

---
+++ src/main/java/studybackend/refrigeratorcleaner/jwt/dto/response/TokenResponse.java
@@ -0,0 +1,16 @@
+package studybackend.refrigeratorcleaner.jwt.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+    private String refreshToken;
+}
