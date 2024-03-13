49f8455e75cb5095e4b156373e3dee2621993037
ehddbs4521 <ehddbs452100@naver.com>
2024-03-13 20:45:26 +0900

    feat: Check RefreshToken If Exists Or Not

--- src/main/java/studybackend/refrigeratorcleaner/repository/TokenRepository.java
+++ src/main/java/studybackend/refrigeratorcleaner/repository/TokenRepository.java
@@ -3,12 +3,8 @@ package studybackend.refrigeratorcleaner.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import studybackend.refrigeratorcleaner.entity.Token;
 
-import java.util.Optional;
-
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
-    Optional<Token> findByAccessToken(String accessToken);
-
-    Optional<Token> findByRefreshToken(String refreshToken);
+    boolean existsByRefreshToken(String refreshToken);
 
 }
