a1d6d6ff7570edfbb287d636d94df29851642394
ehddbs4521 <ehddbs452100@naver.com>
2024-03-16 20:04:52 +0900

    feat: EmailAuthentication Repository

---
+++ src/main/java/studybackend/refrigeratorcleaner/redis/repository/EmailAuthenticationRepository.java
@@ -0,0 +1,11 @@
+package studybackend.refrigeratorcleaner.redis.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import studybackend.refrigeratorcleaner.redis.entity.EmailAuthentication;
+
+import java.util.Optional;
+
+public interface EmailAuthenticationRepository extends CrudRepository<EmailAuthentication, String> {
+
+    Optional<EmailAuthentication> findById(String id);
+}
