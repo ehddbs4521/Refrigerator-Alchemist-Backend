f47b4b58012de8cd822240d7a9d31067c9d1fb53
ehddbs4521 <ehddbs452100@naver.com>
2024-03-16 20:05:11 +0900

    feat: EmailAuthentication Entity

---
+++ src/main/java/studybackend/refrigeratorcleaner/redis/entity/EmailAuthentication.java
@@ -0,0 +1,24 @@
+package studybackend.refrigeratorcleaner.redis.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@AllArgsConstructor
+@RedisHash(timeToLive = 10)
+public class EmailAuthentication {
+
+    @Id
+    @Indexed
+    private String id;
+
+    private String randomNum;
+
+    @TimeToLive
+    private Long exp;
+
+}
