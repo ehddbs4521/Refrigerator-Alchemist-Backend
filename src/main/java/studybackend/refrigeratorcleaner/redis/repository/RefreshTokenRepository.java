af36137c5c838afe0b7a97465eed003fd3762965
ehddbs4521 <ehddbs452100@naver.com>
2024-03-16 20:04:40 +0900

    feat: RefreshToken Repository

---
+++ src/main/java/studybackend/refrigeratorcleaner/redis/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package studybackend.refrigeratorcleaner.redis.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import studybackend.refrigeratorcleaner.redis.entity.RefreshToken;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken,String> {
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+    Optional<RefreshToken> findBySocialId(String socialId);
+}
