9da5dad1344df32a667fb712b371d48f8a47f31c
dongyun <ehddbs452100@naver.com>
2024-03-19 16:49:44 +0900

    fix: Refactoring

--- src/main/java/studybackend/refrigeratorcleaner/controller/AuthController.java
+++ src/main/java/studybackend/refrigeratorcleaner/controller/AuthController.java
@@ -9,17 +9,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import studybackend.refrigeratorcleaner.dto.request.EmailRequest;
-import studybackend.refrigeratorcleaner.dto.request.NickNameRequest;
-import studybackend.refrigeratorcleaner.dto.request.UserRequest;
-import studybackend.refrigeratorcleaner.dto.request.VerifyEmailRequest;
+import studybackend.refrigeratorcleaner.dto.request.*;
 import studybackend.refrigeratorcleaner.jwt.dto.request.SocialIdRequest;
 import studybackend.refrigeratorcleaner.jwt.dto.response.TokenResponse;
 import studybackend.refrigeratorcleaner.jwt.service.JwtService;
 import studybackend.refrigeratorcleaner.service.AuthService;
 
 @Slf4j
+@RequestMapping("/auth")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
@@ -27,28 +26,36 @@ public class AuthController {
     private final AuthService authService;
     private final JwtService jwtService;
 
-    @PostMapping("/auth/send-email")
+    @PostMapping("/send-email")
     public ResponseEntity<Object> sendEmail(@RequestBody EmailRequest emailRequest) {
 
         authService.sendEmail(emailRequest);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/auth/verify-email")
+    @PostMapping("/verify-email")
     public ResponseEntity<Object> verifyEmail(@RequestBody VerifyEmailRequest verifyEmailRequest) {
 
         authService.verifyEmail(verifyEmailRequest);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/auth/resend-email")
+    @PostMapping("/resend-email")
     public ResponseEntity<Object> reSendEmail(@RequestBody EmailRequest emailRequest) {
 
         authService.sendEmail(emailRequest);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/auth/verify-nickname")
+    @PostMapping("/reset-password")
+    public ResponseEntity<Object> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest) {
+
+        authService.resetPassword(resetPasswordRequest);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/verify-nickname")
     public ResponseEntity<Object> verifyNickName(@RequestBody NickNameRequest nickNameRequest) {
 
         authService.verifyNickName(nickNameRequest.getNickName());
@@ -56,7 +63,7 @@ public class AuthController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/auth/signup")
+    @PostMapping("/signup")
     public ResponseEntity<Object> signup(@Valid @RequestBody UserRequest userRequest){
 
         authService.signup(userRequest);
@@ -64,7 +71,7 @@ public class AuthController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/auth/token/logout")
+    @PostMapping("/token/logout")
     public ResponseEntity<Object> logout(HttpServletRequest request,@RequestBody SocialIdRequest socialIdRequest) {
 
         String accessToken = jwtService.extractAccessToken(request).get();
@@ -74,7 +81,7 @@ public class AuthController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/auth/token/reissue")
+    @PostMapping("/token/reissue")
     public ResponseEntity<Object> refresh(HttpServletRequest request, HttpServletResponse response, @RequestBody SocialIdRequest socialIdRequest) {
 
         String refreshToken = jwtService.extractRefreshToken(request).get();
