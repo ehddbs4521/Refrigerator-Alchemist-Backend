4c425ef66882e545b1f6c5705a7b7f5d101f3aa2
dongyun <ehddbs452100@naver.com>
2024-03-21 14:12:20 +0900

    feat: Find By SocialId On BlackListRepository

--- src/main/java/studybackend/refrigeratorcleaner/redis/repository/BlackListRepository.java
+++ src/main/java/studybackend/refrigeratorcleaner/redis/repository/BlackListRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import studybackend.refrigeratorcleaner.redis.entity.BlackList;
 
+import java.util.Optional;
+
 @Repository
 public interface BlackListRepository extends CrudRepository<BlackList, String> {
 
     boolean existsByAccessToken(String accessToken);
+
+    Optional<BlackList> findBySocialId(String socialId);
 }
