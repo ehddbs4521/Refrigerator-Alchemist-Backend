4a74678d7f5357a8d69efb0195e16ebff578b5d3
dongyun <ehddbs452100@naver.com>
2024-03-21 14:11:25 +0900

    feat: Delete Spring Context Data

--- src/main/java/studybackend/refrigeratorcleaner/controller/AuthController.java
+++ src/main/java/studybackend/refrigeratorcleaner/controller/AuthController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -77,7 +78,7 @@ public class AuthController {
         String accessToken = jwtService.extractAccessToken(request).get();
         String refreshToken = jwtService.extractRefreshToken(request).get();
         authService.logout(accessToken, refreshToken, socialIdRequest.getSocialId());
-
+        SecurityContextHolder.clearContext();
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
