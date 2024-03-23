14cd3ff5ad987d93368aa086da84b3f30e01e6d3
dongyun <ehddbs452100@naver.com>
2024-03-23 22:14:48 +0900

    feat: Mockito Test

--- src/test/java/studybackend/refrigeratorcleaner/RefrigeratorCleanerApplicationTests.java
+++ src/test/java/studybackend/refrigeratorcleaner/RefrigeratorCleanerApplicationTests.java
@@ -1,13 +1,6 @@
 package studybackend.refrigeratorcleaner;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class RefrigeratorCleanerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
