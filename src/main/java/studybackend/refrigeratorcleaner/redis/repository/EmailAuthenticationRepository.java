478c2a9ae2635303234d55a78e2e9bfd8cc1e751
dongyun <ehddbs452100@naver.com>
2024-03-27 15:24:32 +0900

    fix: Add Repository Annotation

--- src/main/java/studybackend/refrigeratorcleaner/redis/repository/EmailAuthenticationRepository.java
+++ src/main/java/studybackend/refrigeratorcleaner/redis/repository/EmailAuthenticationRepository.java
@@ -1,10 +1,12 @@
 package studybackend.refrigeratorcleaner.redis.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import studybackend.refrigeratorcleaner.redis.entity.EmailAuthentication;
 
 import java.util.Optional;
 
+@Repository
 public interface EmailAuthenticationRepository extends CrudRepository<EmailAuthentication, String> {
 
     Optional<EmailAuthentication> findById(String id);
